f29020c7e1a6f25a9a1512ff97215d19ca81511e
lucypher66 <danielmenz70@gmail.com>
2021-11-07 01:18:05 +0100

    Added install routine

---
+++ install.py
@@ -0,0 +1,13 @@
+#!/bin/python3
+import os
+
+answer = input("Soll wg-helper installiert werden? [Y/N]: ")
+
+if answer == "y" or answer == "y":
+    os.system("mkdir /etc/wg-helper")
+    os.system("cp -r * /etc/wg-helper")
+    os.system("ln -sf /etc/wg-helper/wg-helper.py /usr/bin/wg-helper")
+    print("wg-helper wurde installiert und kann mit \"sudo wg-helper\" aufgerufen werden!")
+
+else:
+    exit("wg-helper wird nicht installiert.")
