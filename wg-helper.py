cfa28841660cf46c045fc58e1f352c9c3d3f4ae2
lucypher66 <danielmenz70@gmail.com>
2021-11-07 01:56:11 +0100

    Made clean the terminal and then loop itself

--- wg-helper.py
+++ wg-helper.py
@@ -12,46 +12,55 @@ import uninstall
 if os.geteuid() != 0:
     exit("Root-Rechte werden benötigt! Bitte als root- oder sudo-user ausführen!")
 
-print("################################################")
-print("#                                              #")
-print("#                  wg-helper                   #")
-print("#           Making WireGuard simple!           #")
-print("#                                              #")
-print("################################################")
-print("")
-print("1. Neuen Server anlegen")
-print("2. Neuen Peer hinzufügen")
-print("3. Peer autorisieren")
-print("4. Peer deautoriseren")
-print("5. Tunnel-Interface löschen")
-print("6. wg-helper aktualisieren")
-print("7. wg-helper deinstallieren")
-print("8. Programm beenden")
-print("")
-answer = input("Auswahl: ")
+run = True
 
-if answer == "1":
-    add_server_interface.generate_interface_config()
+def menu():
 
-if answer == "2":
-    add_peer_interface.generate_interface_config()
+    print("################################################")
+    print("#                                              #")
+    print("#                  wg-helper                   #")
+    print("#           Making WireGuard simple!           #")
+    print("#                                              #")
+    print("################################################")
+    print("")
+    print("1. Neuen Server anlegen")
+    print("2. Neuen Peer hinzufügen")
+    print("3. Peer autorisieren")
+    print("4. Peer deautoriseren")
+    print("5. Tunnel-Interface löschen")
+    print("6. wg-helper aktualisieren")
+    print("7. wg-helper deinstallieren")
+    print("8. Programm beenden")
+    print("")
+    answer = input("Auswahl: ")
 
-if answer == "3":
-    authorize_peer_on_server.add_peer()
+    if answer == "1":
+        add_server_interface.generate_interface_config()
 
-if answer == "4":
-    deauthorize_peer_on_server.remove_peer()
+    if answer == "2":
+        add_peer_interface.generate_interface_config()
 
-if answer == "5":
-    delete_interface.delete_interface()
+    if answer == "3":
+        authorize_peer_on_server.add_peer()
 
-if answer == "6":
-    os.system("cd /etc/wg-helper/")
-    os.system("git pull")
-    print("wg-helper ist auf dem aktuellsten Stand!")
+    if answer == "4":
+        deauthorize_peer_on_server.remove_peer()
 
-if answer == "7":
-    uninstall.uninstall()
+    if answer == "5":
+        delete_interface.delete_interface()
 
-if answer == "8":
-    exit("wg-helper wird beendet.")
+    if answer == "6":
+        os.system("cd /etc/wg-helper/")
+        os.system("git pull")
+        print("wg-helper ist auf dem aktuellsten Stand!")
+
+    if answer == "7":
+        uninstall.uninstall()
+
+    if answer == "8":
+        exit("wg-helper wird beendet.")
+
+os.system("clear")
+
+while run == True:
+    menu()
