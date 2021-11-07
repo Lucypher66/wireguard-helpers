e777a88e0109f9eeb90e9b2b0b43b6aeb6a39829
lucypher66 <danielmenz70@gmail.com>
2021-11-07 01:22:52 +0100

    Syntax errors fixed

--- uninstall.py
+++ uninstall.py
@@ -5,9 +5,9 @@ def uninstall():
     answer = input("Soll wg-helper deinstalliert werden? [Y/N]: ")
 
     if answer == "y" or answer == "y":
-    os.system("rm -rf /etc/wg-helper")
-    os.system("rm /usr/bin/wg-helper")
-    print("wg-helper wurde deinstalliert!")
+        os.system("rm -rf /etc/wg-helper")
+        os.system("rm /usr/bin/wg-helper")
+        print("wg-helper wurde deinstalliert!")
 
     else:
-    exit("Deinstallation abgebrochen")
+        exit("Deinstallation abgebrochen")
