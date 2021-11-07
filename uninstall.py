b41f3195750ee422d5c6f701d13f12859b7c5ea9
lucypher66 <danielmenz70@gmail.com>
2021-11-07 01:23:27 +0100

    Bugfixes

--- uninstall.py
+++ uninstall.py
@@ -4,7 +4,7 @@ import os
 def uninstall():
     answer = input("Soll wg-helper deinstalliert werden? [Y/N]: ")
 
-    if answer == "y" or answer == "y":
+    if answer == "y" or answer == "Y":
         os.system("rm -rf /etc/wg-helper")
         os.system("rm /usr/bin/wg-helper")
         print("wg-helper wurde deinstalliert!")
