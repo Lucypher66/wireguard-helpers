1adf4c2bfde63483de5e12fa95b81225b32fcec4
lucypher66 <danielmenz70@gmail.com>
2021-11-11 20:36:53 +0100

    Bugfixes

--- add_peer_interface.py
+++ add_peer_interface.py
@@ -50,22 +50,22 @@ def generate_interface_config():
         interface_public_key = pubkey.read().rstrip()
 
     #Prepare interface strings for input
-    insert_interface_prefix = "[Interface]"
-    insert_interface_address = "Address = " + interface_ip_and_subnetmask
-    insert_save_config = "SaveConfig = True"
-    insert_listenport = "ListenPort = " + local_listen_port
-    insert_privatekey = "PrivateKey = " + interface_private_key
+    insert_interface_prefix = "[Interface]" + "\n"
+    insert_interface_address = "Address = " + interface_ip_and_subnetmask + "\n"
+    insert_save_config = "SaveConfig = True" + "\n"
+    insert_listenport = "ListenPort = " + local_listen_port + "\n"
+    insert_privatekey = "PrivateKey = " + interface_private_key + "\n"
 
     #Prepare empty line string for beauty purposes
     insert_empty_line = ""
 
     #Prepare peer strings for input
-    insert_peer_prefix = "[Peer]"
-    insert_peer_public_key = "PublicKey = " + peer_pubkey
-    insert_tunnel_ips = "echo \"AllowedIPs = " + tunnel_ips + "/" + tunnel_subnet
-    insert_peer_ip_and_port = "echo \"Endpoint = " + peer_ip_or_domain + ":" + peer_listen_port
+    insert_peer_prefix = "[Peer]" + "\n"
+    insert_peer_public_key = "PublicKey = " + peer_pubkey + "\n"
+    insert_tunnel_ips = "AllowedIPs = " + tunnel_ips + "/" + tunnel_subnet + "\n"
+    insert_peer_ip_and_port = "Endpoint = " + peer_ip_or_domain + ":" + peer_listen_port + "\n"
     if set_keepalive == "y" or set_keepalive == "Y":
-        insert_peer_keepalive = "echo \"PersistentKeepalive = " + keepalive_seconds
+        insert_peer_keepalive = "PersistentKeepalive = " + keepalive_seconds  + "\n"
 
     #Insert interface config
     with open (interface_conf_path,"w") as conf_file:
