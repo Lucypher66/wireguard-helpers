7286511d7bf005d9a10ba8e6b5866746e9ddf8ee
Lucypher66 <51540565+Lucypher66@users.noreply.github.com>
2022-02-25 12:20:11 +0100

    Add files via upload

---
+++ deauthorize_peer_on_server.py
@@ -0,0 +1,20 @@
+#!/bin/python3
+import os
+
+def remove_peer():
+
+    #Root check
+    if os.geteuid() != 0:
+        exit("Root-Rechte werden benötigt! Bitte als root- oder sudo-user ausführen!")
+
+    #Gather interface name
+    interface = input("WireGuard-Interface angeben: ")
+
+    #Gather the pubkey of the peer that will be removed
+    pubkey = input("Public Key des Peers eingeben: ")
+
+    #Assemble wireguard command
+    wireguard_command = "wg set " + interface + " peer " + pubkey + " remove"
+
+    #Execute wireguard command
+    os.system(wireguard_command)
---
+++ gui.py
@@ -0,0 +1,8 @@
+from tkinter import *
+window = Tk()
+window.title("WireGuard-Helpers")
+window.geometry("480x768")
+
+
+
+window.mainloop()
